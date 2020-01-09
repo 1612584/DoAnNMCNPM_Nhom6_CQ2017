11cd7f4b52c90984f0de7589ffb66e6d71cea6ca
huynhthanhsang21750150 <huynhthanhsang1771@gmail.com>
2020-01-10 04:07:36 +0700

    sửa giao diện input file ds vđv

--- models/player.model.js
+++ models/player.model.js
@@ -1,10 +1,15 @@
 const db = require("../utils/db.js");
 
 module.exports = {
-  add: async player => {
-    const query = `insert into VANDONGVIEN 
-                        (name, birthDay, skill, tenToChuc)
-                        values ('${player.name}', '${player.birthDay}', '${player.skill}','${player.tenToChuc}')`;
-    const result = await db.load(query);
+  //   add: async player => {
+  //     const query = `insert into VANDONGVIEN
+  //                         (name, birthDay, skill, tenToChuc)
+  //                         values ('${player.name}', '${player.birthDay}', '${player.skill}','${player.tenToChuc}')`;
+  //     const result = await db.load(query);
+  //     return result[0].insertedId;
+  //   }
+  add: async entity => {
+    const result = await db.add("VANDONGVIEN", entity);
+    return result.insertedId;
   }
 };
--- routes/managers/inputPlayerList.js
+++ routes/managers/inputPlayerList.js
@@ -4,80 +4,80 @@ var moment = require("moment");
 const playerModel = require("../../models/player.model");
 
 const parseDateExcel = excelTimestamp => {
-  const secondsInDay = 24 * 60 * 60;
-  const excelEpoch = new Date(1899, 11, 31);
-  const excelEpochAsUnixTimestamp = excelEpoch.getTime();
-  const missingLeapYearDay = secondsInDay * 1000;
-  const delta = excelEpochAsUnixTimestamp - missingLeapYearDay;
-  const excelTimestampAsUnixTimestamp = excelTimestamp * secondsInDay * 1000;
-  const parsed = excelTimestampAsUnixTimestamp + delta;
-  return isNaN(parsed) ? null : parsed;
+    const secondsInDay = 24 * 60 * 60;
+    const excelEpoch = new Date(1899, 11, 31);
+    const excelEpochAsUnixTimestamp = excelEpoch.getTime();
+    const missingLeapYearDay = secondsInDay * 1000;
+    const delta = excelEpochAsUnixTimestamp - missingLeapYearDay;
+    const excelTimestampAsUnixTimestamp = excelTimestamp * secondsInDay * 1000;
+    const parsed = excelTimestampAsUnixTimestamp + delta;
+    return isNaN(parsed) ? null : parsed;
 };
 
 const storage = multer.diskStorage({
-  filename: function(req, file, cb) {
-    cb(null, "sheet.xlsx");
-  },
-  destination: function(req, file, cb) {
-    cb(null, `./public/lists/`);
-  }
+    filename: function (req, file, cb) {
+        cb(null, "sheet.xlsx");
+    },
+    destination: function (req, file, cb) {
+        cb(null, `./public/lists/`);
+    }
 });
 const upload = multer({
-  storage
+    storage
 });
 
 module.exports.get = (req, res) => {
-  res.render("managers/inputPlayerList", {
-    layout: "manager"
-  });
+    res.render("managers/inputPlayerList", {
+        layout: "manager"
+    });
 };
 
 module.exports.post = async (req, res) => {
-  await upload.single("file-input")(req, res, err => {
-    if (err) {
-    }
-    var data = [];
-    var workbook = XLSX.readFile("./public/lists/sheet.xlsx");
-    var sheet_name_list = workbook.SheetNames;
-    sheet_name_list.forEach(function(y) {
-      var worksheet = workbook.Sheets[y];
-      var headers = {};
-      for (z in worksheet) {
-        if (z[0] === "!") continue;
-        //parse out the column, row, and value
-        var tt = 0;
-        for (var i = 0; i < z.length; i++) {
-          if (!isNaN(z[i])) {
-            tt = i;
-            break;
-          }
-        }
-        var col = z.substring(0, tt);
-        var row = parseInt(z.substring(tt));
-        var value = worksheet[z].v;
+    await upload.single("file-input")(req, res, err => {
+        if (err) {}
+        var data = [];
+        var workbook = XLSX.readFile("./public/lists/sheet.xlsx");
+        var sheet_name_list = workbook.SheetNames;
+        sheet_name_list.forEach(function (y) {
+            var worksheet = workbook.Sheets[y];
+            var headers = {};
+            for (z in worksheet) {
+                if (z[0] === "!") continue;
+                //parse out the column, row, and value
+                var tt = 0;
+                for (var i = 0; i < z.length; i++) {
+                    if (!isNaN(z[i])) {
+                        tt = i;
+                        break;
+                    }
+                }
+                var col = z.substring(0, tt);
+                var row = parseInt(z.substring(tt));
+                var value = worksheet[z].v;
 
-        //store header names
-        if (row == 1 && value) {
-          headers[col] = value;
-          continue;
-        }
+                //store header names
+                if (row == 1 && value) {
+                    headers[col] = value;
+                    continue;
+                }
 
-        if (!data[row]) data[row] = {};
-        data[row][headers[col]] = value;
-      }
-      //drop those first two rows which are empty
-      data.shift();
-      data.shift();
-      console.log(data);
+                if (!data[row]) data[row] = {};
+                data[row][headers[col]] = value;
+            }
+            //drop those first two rows which are empty
+            data.shift();
+            data.shift();
+            console.log(data);
+        });
+        data.forEach(async element => {
+            delete element.id;
+            element.birthDay = new Date(parseDateExcel(element.birthDay));
+            element.birthDay = moment(element.birthDay).format("YYYY-MM-DD");
+            const result = await playerModel.add(element);
+        });
+        res.render("managers/inputPlayerList", {
+            message: "Đã nhập danh sách vận động viên từ file thành công!",
+            layout: "manager"
+        });
     });
-    data.forEach(async element => {
-      delete element.id;
-      element.birthDay = new Date(parseDateExcel(element.birthDay));
-      element.birthDay = moment(element.birthDay).format("YYYY-MM-DD");
-      const rows = await playerModel.add(element);
-    });
-    res.render("manager/inputPlayerList", {
-      message: "Đã nhập danh sách vận động viên từ file thành công!"
-    });
-  });
 };
